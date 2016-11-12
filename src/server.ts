ef04e849b4b3c8db3c766cc1d2609ad5e1888bdc
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-11-12 21:07:28 +0100

    added draft of 80 listener

--- src/server.ts
+++ src/server.ts
@@ -108,6 +108,12 @@ let server = spdy.createServer({
     console.log('Listening on port: ' + app.get('port'));
   });
 
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80);
+
 function ngApp(req, res) {
 
   const config = {
