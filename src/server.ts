f66e60172a17d979c2c32452df23f06b477f872a
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-11-12 22:26:17 +0100

    updated style

--- src/server.ts
+++ src/server.ts
@@ -112,7 +112,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
   res.end();
-}).listen(80);
+}).listen(8080);
 
 function ngApp(req, res) {
 
