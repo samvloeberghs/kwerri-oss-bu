a9d9c23518f508f4d552b6127911353e4ff2b719
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2016-11-20 19:19:18 +0100

    updated

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ import { createEngine } from 'angular2-express-engine';
 
 const minify = require('html-minifier').minify;
 const minifyOptions = require('./options').htmlMinifyOptions;
-const serverConfig = require('./options').serverConfig.universalNoCache;
+const serverConfig = require('./options').serverConfig.universalFileCache;
 const spdy = require('spdy');
 const fs = require('fs');
 
