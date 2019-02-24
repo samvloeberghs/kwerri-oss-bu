c8f9a9bb0af004d06d693b027a520cf3e7dac5d8
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2019-02-24 22:15:17 +0100

    remove compression

--- projects/samvloeberghs-server/src/index.ts
+++ projects/samvloeberghs-server/src/index.ts
@@ -72,6 +72,7 @@ if (PROD) {
   app.use(cors(corsOptions));
 }
 
+/*
 app.use(compression({
   filter: (req, res) => {
     let ignore = false;
@@ -84,6 +85,7 @@ app.use(compression({
     return ignore ? false : compression.filter(req, res);
   },
 }));
+*/
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
