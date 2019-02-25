07f05ee18ee79c12203e3d5dda6cba037dadc1fc
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2019-02-25 19:45:51 +0100

    other cors options

--- projects/samvloeberghs-server/src/cache.config.ts
+++ projects/samvloeberghs-server/src/cache.config.ts
@@ -1,6 +1,6 @@
 const allowedBlogPaths = [
   'posts/whats-with-the-subjects-in-rxjs5',
-  'posts/better-seo-for-social-media-platforms-with-angular-universal',
+  'posts/better-sharing-on-social-media-platforms-with-angular-universal',
   'posts/scroll-to-top-on-angular-router-navigation',
   'posts/selecting-subtitles-in-vimeo-using-javascript',
 ];
