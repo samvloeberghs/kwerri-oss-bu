ae6bb129808d6e1c566160eed487acc83c6a6549
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2019-03-14 12:36:16 +0100

    updated

--- projects/samvloeberghs-server/src/cache.config.ts
+++ projects/samvloeberghs-server/src/cache.config.ts
@@ -1,7 +1,7 @@
 const allowedBlogPaths = [
   'posts/whats-with-the-subjects-in-rxjs5',
   'posts/better-sharing-on-social-media-platforms-with-angular-universal',
-  'posts/outputting-ngx-sv-json-ld-with-angular-universal',
+  'posts/outputting-json-ld-with-angular-universal',
   'posts/scroll-to-top-on-angular-router-navigation',
   'posts/selecting-subtitles-in-vimeo-using-javascript',
 ];
