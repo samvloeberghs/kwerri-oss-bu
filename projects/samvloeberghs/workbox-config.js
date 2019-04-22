e76f9395275a790c2cde7412da23772f672641e3
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2019-04-22 11:31:29 +0200

    updated svg

--- projects/samvloeberghs/workbox-config.js
+++ projects/samvloeberghs/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: "dist/samvloeberghs/browser/",
   globPatterns: [
-    "**/*.{js,html,pdf,css,png,jpg,ico,json}"
+    "**/*.{js,html,pdf,css,png,jpg,svg,ico,json}"
   ],
   swSrc: './projects/samvloeberghs/src/sw.js',
   swDest: './dist/samvloeberghs/browser/sw.js'
