4c85b40c5c224bab744f2a0504aaa1396a9a4ff5
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2019-07-12 16:30:18 +0200

    show last update

--- projects/samvloeberghs/src/app/app-routing.module.ts
+++ projects/samvloeberghs/src/app/app-routing.module.ts
@@ -41,7 +41,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled',
-    scrollPositionRestoration: 'enabled'
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled'
   })],
   exports: [RouterModule],
 })
