e3abb80b9799ec9cb2fbaa32d033737752487fe8
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-01-05 00:54:02 +0100

    progress

--- projects/samvloeberghs/src/app/shared/highlight.service.ts
+++ projects/samvloeberghs/src/app/shared/highlight.service.ts
@@ -7,6 +7,8 @@ import 'prismjs';
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
 import 'prismjs/plugins/line-highlight/prism-line-highlight';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-typescript';
