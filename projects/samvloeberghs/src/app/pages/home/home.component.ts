128de5208104e9bcacf8ca3c12fe1cd2589ad8a5
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-01-06 17:12:20 +0100

    updated homepage

--- projects/samvloeberghs/src/app/pages/home/home.component.ts
+++ projects/samvloeberghs/src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { DataService } from '../../shared/data.service';
+import { Post } from '../posts/post/post.model';
 
 @Component({
   selector: 'sv-home',
@@ -6,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+
+  public readonly lastPost$: Observable<Post> = this.dataService
+    .getData('/posts/data.json')
+    .pipe(
+      map(response => response[0] as Post),
+    );
+
+  constructor(
+    private readonly dataService: DataService
+  ) {
+  }
 
 }
--- projects/samvloeberghs/src/app/pages/posts/posts.component.ts
+++ projects/samvloeberghs/src/app/pages/posts/posts.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 import { Post } from './post/post.model';
 import { DataService } from '../../shared/data.service';
@@ -11,29 +12,23 @@ import { DataService } from '../../shared/data.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
 
-  posts: Post[];
-  error: any;
+  public readonly posts$: Observable<Post[]> = this.dataService
+    .getData('/posts/data.json')
+    .pipe(
+      map(response => response as Post[]),
+      tap({
+        error: (_: HttpErrorResponse) => {
+          this.router.navigateByUrl('/not-found');
+        },
+      }),
+    );
 
   constructor(
     private readonly dataService: DataService,
-    private readonly router: Router
+    private readonly router: Router,
   ) {
-  }
-
-  ngOnInit() {
-    this.dataService
-      .getData('/posts/data.json')
-      .pipe(
-        map(response => response as Post[]),
-      )
-      .subscribe(
-        posts => this.posts = posts,
-        (error: HttpErrorResponse) => {
-          this.router.navigateByUrl('/not-found');
-        },
-      );
   }
 
 }
