627aa257ad54561ad7d32f709d9f3957d6e798af
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-02-02 21:23:52 +0100

    updated scully disable angular

--- projects/scully-disable-angular/src/lib/index.ts
+++ projects/scully-disable-angular/src/lib/index.ts
@@ -3,21 +3,35 @@ import { scullyConfig } from '@scullyio/scully/utils/config';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+const escapeRegExp = (string): string => {
+  // $& means the whole matched string
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+
 const disableAngularPlugin = async (html) => {
-  const statsJsonPath = join(scullyConfig.distFolder, 'stats.json');
+  const tsConfigPath = 'tsconfig.json';
+  const tsConfig = JSON.parse(readFileSync(tsConfigPath, { encoding: 'utf8' }).toString());
+
+  let isEs5Config = false;
+  let statsJsonPath = join(scullyConfig.distFolder, 'stats-es2015.json');
+  if (tsConfig.compilerOptions.target === 'es5') {
+    isEs5Config = true;
+    statsJsonPath = join(scullyConfig.distFolder, 'stats.json');
+  }
+
   if (!existsSync(statsJsonPath)) {
-    const noStatsJsonError = `A stats.json is required for the 'disableAngular' plugin.
+    const noStatsJsonError = `A ${isEs5Config ? 'stats' : 'stats-es2015'}.json is required for the 'disableAngular' plugin.
 Please run 'npm build' with the '--stats-json' flag`;
     console.error(noStatsJsonError);
     throw new Error(noStatsJsonError);
   }
 
   const scullyDisableAngularStatsJsonPath = join(scullyConfig.distFolder, 'scully-disable-angular-stats.json');
-  let scullyDisableAngularStatsJson = '';
+  let scullyDisableAngularStatsJson = [];
   if (!existsSync(scullyDisableAngularStatsJsonPath)) {
     const errorCreatingScullyDisableAngularStatsJsonError = 'The scully-disable-angular-stats.json could not be created';
     try {
-      scullyDisableAngularStatsJson = JSON.parse(readFileSync(statsJsonPath, { encoding: 'utf8' }).toString()).assetsByChunkName;
+      scullyDisableAngularStatsJson = JSON.parse(readFileSync(statsJsonPath, { encoding: 'utf8' }).toString()).assets;
       writeFileSync(scullyDisableAngularStatsJsonPath, JSON.stringify(scullyDisableAngularStatsJson));
     } catch (e) {
       console.error(e);
@@ -28,8 +42,20 @@ Please run 'npm build' with the '--stats-json' flag`;
     scullyDisableAngularStatsJson = JSON.parse(readFileSync(scullyDisableAngularStatsJsonPath, { encoding: 'utf8' }).toString());
   }
 
-  Object.values(scullyDisableAngularStatsJson).map(entry => entry[0]).forEach(entry => {
-    html = html.replace(new RegExp(`<script src="?${entry}"?( nomodule(="")?)? defer><\/script>`, 'gmi'), '');
+  let assetsList = scullyDisableAngularStatsJson.filter(entry => {
+    return entry['name'].includes('.js') && (
+      entry['name'].includes('-es5') || entry['name'].includes('-es2015')
+    );
+  }).map(entry => entry['name']);
+  assetsList = [...assetsList, ...assetsList.map(asset => {
+    return asset.includes('-es5') ?
+      asset.replace('-es5', '-es2015') :
+      asset.replace('-es2015', '-es5');
+  })];
+
+  assetsList.forEach(entry => {
+    const regex = new RegExp(`<script( charset="?utf-8"?)? src="?${escapeRegExp(entry)}"?( type="?module"?)?( nomodule(="")?)?( defer(="")?)?><\/script>`, 'gmi');
+    html = html.replace(regex, '');
   });
   return Promise.resolve(html);
 };
