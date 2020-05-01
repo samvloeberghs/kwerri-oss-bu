cc407c07322a462c7e19c37240ddfe0d87eb6ec3
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-05-01 14:05:50 +0200

    add GIF to glob pattern workbox config

--- projects/samvloeberghs/workbox-config.js
+++ projects/samvloeberghs/workbox-config.js
@@ -5,7 +5,7 @@ module.exports = {
     'posts/**/post.html',
   ],
   globPatterns: [
-    "**/*.{js,html,pdf,css,png,jpg,svg,ico,json}"
+    "**/*.{js,html,pdf,css,png,jpg,svg,ico,json,gif}"
   ],
   swSrc: './projects/samvloeberghs/src/sw.js',
   swDest: './dist/samvloeberghs/browser/sw.js'
