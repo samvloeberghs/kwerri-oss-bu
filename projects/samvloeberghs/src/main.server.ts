a0df44e52e0e01e401287768f7693d7009d4bc64
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-07-01 13:47:09 +0200

    updated code comments

--- projects/samvloeberghs/src/main.server.ts
+++ projects/samvloeberghs/src/main.server.ts
@@ -1,5 +1,5 @@
-import { environment } from './environments/environment';
 import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
