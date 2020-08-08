b4554a23793da93adc3cfb02a1bf76f7dae246d5
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2020-08-08 18:22:18 +0200

    updated a lot

--- projects/samvloeberghs-sw/src/index.ts
+++ projects/samvloeberghs-sw/src/index.ts
@@ -1,10 +1,12 @@
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
 import { initialize } from 'workbox-google-analytics';
+import { registerRoute } from 'workbox-routing';
 
 declare const self: any;
 
 // Precache & Route setup
 cleanupOutdatedCaches();
+
 // Keep it here or it will not get picked up
 // see workbox-config.js
 // This array gets injected automagically by the workbox cli
@@ -16,6 +18,22 @@ precacheAndRoute(assetsToCache);
 // Google Analytics cache setup
 // see https://developers.google.com/web/tools/workbox/modules/workbox-google-analytics
 initialize();
+
+// Catch navigation requests
+registerRoute(
+  ({ event }) => event.request.mode === 'navigate',
+  async ({ request }: { request: Request }) => {
+    return caches
+      .match(request)
+      .then(response => {
+        return response || fetch(request);
+      })
+      .catch(err => {
+        console.error(err);
+        return fetch(request);
+      });
+  },
+);
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
